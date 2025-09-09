d1c68a2400855686b41430b998a222923d34645a
leo <leomegnigbeto@gmail.com>
2025-09-09 22:19:20 +0100

    custome

--- src/components/gallery/GalleryView.tsx
+++ src/components/gallery/GalleryView.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { Media, MasonryGrid } from "@once-ui-system/core";
 import { gallery } from "@/resources";
 
@@ -7,16 +8,49 @@ export default function GalleryView() {
   return (
     <MasonryGrid columns={2} s={{ columns: 1 }}>
       {gallery.images.map((image, index) => (
-        <Media
-          enlarge
-          priority={index < 10}
-          sizes="(max-width: 560px) 100vw, 50vw"
+        <div
           key={index}
-          radius="m"
-          aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
-          src={image.src}
-          alt={image.alt}
-        />
+          style={{ position: "relative", cursor: "pointer" }}
+        >
+          <Media
+            enlarge
+            priority={index < 10}
+            sizes="(max-width: 560px) 100vw, 50vw"
+            radius="m"
+            aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
+            src={image.src}
+            alt={image.alt}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              background: "rgba(0,0,0,0.5)",
+              color: "#fff",
+              fontSize: "1.1rem",
+              fontWeight: 500,
+              textAlign: "center",
+              opacity: 0,
+              transition: "opacity 0.2s",
+              pointerEvents: "none",
+              zIndex: 2,
+            }}
+            className="gallery-alt-overlay"
+          >
+            {image.alt}
+          </div>
+          <style>{`
+            .gallery-alt-overlay:hover, .gallery-alt-overlay:active, div:hover > .gallery-alt-overlay {
+              opacity: 1 !important;
+            }
+          `}</style>
+        </div>
       ))}
     </MasonryGrid>
   );
